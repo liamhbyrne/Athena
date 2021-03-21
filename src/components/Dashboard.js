8b7828520883fda363614f9043cc62ff69519d4d
FireTramp <lucaswarwick02@gmail.com>
2021-03-21 01:32:49 +0000

    Backup Dashboard Page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,31 +6,57 @@ import "../css/Dashboard.css"
 import ReactDOM from 'react-dom'
 import { auth, db}  from '../firebase'
 
-// Display Functions
-function resetContent() {
-  var mainContent = document.getElementById("mainContent");
-  while (mainContent.lastElementChild) {
-    mainContent.removeChild(mainContent.lastElementChild);
+var currentColumn = 0;
+
+function nextColumn() {
+  if (currentColumn + 1 == 4) {
+    currentColumn = 0;
+  }
+  else {
+    currentColumn++;
   }
 }
 
+// Display Functions
+function resetContent() {
+  var mainContent = document.getElementById("mainContent");
+  var i;
+  for (i = 0; i < 4; i++) {
+    while (mainContent.children[i].lastElementChild) {
+      mainContent.children[i].removeChild(mainContent.children[i].lastElementChild);
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Open functions
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 // Display all the users Active Tasks
-async function openActiveTasks() {
+async function openSetByMe() {
+  currentColumn = 0;
   resetContent();
   const taskRef = db.collection("task");
   const myTasks = await taskRef.where("userUID", "==", auth.currentUser.uid).get();
   myTasks.forEach(doc => {
-    createActiveTask(doc.get("priority"), doc.get("name"), doc.get("desc"));
+    createSetByMeTask(doc.get("priority"), doc.get("name"), doc.get("desc"), doc.get("skills"));
   })
-  var mainDiv = document.getElementById("mainContent");
-  var tempDiv = document.createElement("div");
-  tempDiv.id = 69;
-  mainDiv.appendChild(tempDiv);
+}
+
+async function openActiveTasks() {
+  currentColumn = 0;
+  resetContent();
+  const taskRef = db.collection("task");
+  const myTasks = await taskRef.where("recipientUID", "==", auth.currentUser.uid).get();
+  myTasks.forEach(doc => {
+    createActiveTask(doc.get("priority"), doc.get("name"), doc.get("desc"), doc.get("skills"));
+  })
 }
 
 // Display the current Task Pool
 async function openTaskPool() {
-  console.log("attemptings to open task pool");
+  currentColumn = 0;
   resetContent();
   const taskRef = db.collection("task");
   const myTasks = await taskRef.where("userUID", "!=", auth.currentUser.uid).get();
@@ -39,16 +65,21 @@ async function openTaskPool() {
   });
 }
 
-// Creates each individual Active Task 
-var activeTaskCount = 0;
-function createActiveTask (priority, name, desc) {
-  var elements = <div class={`task-container ${priority}`}>
-    <div class="task-name">{name}</div>
-    <div class="task-body">
-      <div class="task-desc">{desc}</div>
-    </div>
-  </div>;
-  var mainDiv = document.getElementById("mainContent");
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Create functions
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Creates each individual Set by Me Task
+var setByMeTaskCount = 0;
+function createSetByMeTask (priority, name, desc, skills) {
+  if(desc.length > 150){
+    desc = desc.substring(0, 150) + "...";
+  }
+  var elements = getTaskBox(priority, name, desc, skills);
+  var mainDiv = document.getElementById("mainContent").children[currentColumn];
+  nextColumn();
   var tempDiv = document.createElement("div");
   tempDiv.id = activeTaskCount;
   mainDiv.appendChild(tempDiv);
@@ -57,29 +88,33 @@ function createActiveTask (priority, name, desc) {
   activeTaskCount++;
 }
 
+// Creates each individual Active Task 
+var activeTaskCount = 0;
+function createActiveTask (priority, name, desc, skills) {
+  if(desc.length > 150){
+    desc = desc.substring(0, 150) + "...";
+  }
+  var elements = getTaskBox(priority, name, desc, skills);
+  var mainDiv = document.getElementById("mainContent").children[currentColumn];
+  nextColumn();
+  var tempDiv = document.createElement("div");
+  tempDiv.id = activeTaskCount;
+  mainDiv.appendChild(tempDiv);
+  mainDiv.appendChild(document.createElement("p"));
+  ReactDOM.render(elements, document.getElementById(activeTaskCount));
+  activeTaskCount++;
+}
 
-// Creates each individual Pool Task
 var poolTaskCount = 0;
 var colorMap = ["#ff0460", "#cbdc56", "#64a3ea", 	"#ffc100", "#c356ea", "#8ff243", "#71aef2", "#ea5645"];
+// Creates each individual Pool Task
 function createPoolTask(priority, name, desc, skills) {
   if(desc.length > 150){
     desc = desc.substring(0, 150) + "...";
   }
-
-  console.log(typeof skills, skills);
-  var elements = <div class={`task-container ${priority}`}>
-
-    <div class="task-name">{name}</div>
-    <div class="task-body">
-      <div class="task-desc">{desc}</div>
-      <div class="task-skills">
-        {skills.map(skill => (
-          <div class="task-skill" style={{backgroundColor: colorMap[skill.length % colorMap.length]}}>{skill}</div>
-        ))}
-      </div>
-    </div>
-  </div>;
-  var mainDiv = document.getElementById("mainContent");
+  var elements = getTaskBox(priority, name, desc, skills);
+  var mainDiv = document.getElementById("mainContent").children[currentColumn];
+  nextColumn();
   var tempDiv = document.createElement("div");
   tempDiv.id = poolTaskCount;
   mainDiv.appendChild(tempDiv);
@@ -87,6 +122,12 @@ function createPoolTask(priority, name, desc, skills) {
   ReactDOM.render(elements, document.getElementById(poolTaskCount));
   poolTaskCount++;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Other functions
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 // Calculates a percentage for how relavent the task is for the currentUser
 function calculateRelevancy (userSkills, requiredSkils) {
@@ -102,19 +143,27 @@ function calculateRelevancy (userSkills, requiredSkils) {
   return (Math.round((same / total) * 100) / 100) * 100
 }
 
-// Full screen display of the Task
-function displayLargePoolTask (email, name, desc, skills) {
-  var elements = <div id="largePoolTask">
-    <h3>{name}</h3>
-    <p>{email}</p>
-    <p>{desc}</p>
-    <p>X</p>
-  </div>
+function getTaskBox (priority, name, desc, skills) {
+  var elements = <div class={`task-container ${priority}`}>
+    <div class="task-name">{name}</div>
+    <div class="task-body">
+      <div class="task-desc">{desc}</div>
+      <div class="task-skills">
+        {skills.map(skill => (
+          <div class="task-skill" style={{backgroundColor: colorMap[skill.length % colorMap.length]}}>{skill}</div>
+        ))}
+      </div>
+    </div>
+  </div>;
   return elements;
 }
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 // Main Code
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 export default function Dashboard() {
   const [setError] = useState("")
   const { logout } = useAuth()
@@ -134,21 +183,26 @@ export default function Dashboard() {
 
 
   return (
-
       <>
         <Card>
           <Card.Body>
             <div class="tabBar">
-
-              <div id="tab" onClick={openActiveTasks}>
+              <div id="tab" onClick={openSetByMe}>
                 <h1>Set By Me</h1>
+              </div>
+              <div id="tab" onClick={openActiveTasks}>
+                <h1>My Tasks</h1>
               </div>
               <div id="tab" onClick={openTaskPool}>
                 <h1>Task Pool</h1>
               </div>
             </div>
-
-            <div id="mainContent" onLoad={openActiveTasks}></div>
+            <div id="mainContent">
+              <div id="column1"></div>
+              <div id="column2"></div>
+              <div id="column3"></div>
+              <div id="column4"></div>
+            </div>
           </Card.Body>
         </Card>
       </>
